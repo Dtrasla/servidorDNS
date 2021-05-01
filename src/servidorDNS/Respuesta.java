05bc8838973186c9b671e207537db0f9b0d30df7
Juan Daniel Traslavina <danstervan1@gmail.com>
2021-05-01 16:23:20 -0500

    Clase Respuesta

--- src/servidorDNS/Respuesta.java
+++ src/servidorDNS/Respuesta.java
@@ -1,9 +1,12 @@
 package servidorDNS;
 
-
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
 
 /*
-All RRs have the same top level format shown below:
+All RRs (Resource Record) have the same top level format shown below:
 
                                     1  1  1  1  1  1
       0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
@@ -24,11 +27,75 @@ All RRs have the same top level format shown below:
     +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--|
     /                     RDATA                     /
     /                                               /
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+ */
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    
+	NAME            a domain name to which this resource record pertains.
+	
+	TYPE            two octets containing one of the RR type codes.  This
+	                field specifies the meaning of the data in the RDATA
+	                field.
+	
+	CLASS           two octets which specify the class of the data in the
+	                RDATA field.
+	
+	TTL             a 32 bit unsigned integer that specifies the time
+	                interval (in seconds) that the resource record may be
+	                cached before it should be discarded.  Zero values are
+	                interpreted to mean that the RR can only be used for the
+	                transaction in progress, and should not be cached.
+	
+	RDLENGTH        an unsigned 16 bit integer that specifies the length in
+	                octets of the RDATA field.
+	
+	RDATA           a variable length string of octets that describes the
+	                resource.  The format of this information varies
+	                according to the TYPE and CLASS of the resource record.
+	                For example, the if the TYPE is A and the CLASS is IN,
+	                the RDATA field is a 4 octet ARPA Internet address.
+	
+	     *
+  */
 
 
 public class Respuesta {
+	private short name;
+	private short type;
+	private short clase;
+	private int ttl; //Java no tiene unsigned
+	private short rdlength;
+	private InetAddress address;
 	
+	public Respuesta(short name, short type, short clase, int ttl, short rdlength, InetAddress address) {
+		this.name = name;
+		this.type = type;
+		this.clase = clase;
+		this.ttl = ttl;
+		this.rdlength = rdlength;
+		this.address = address;
+	}
+	
+	public byte[] devolverStream(){
+		ByteArrayOutputStream o = new ByteArrayOutputStream();
+		DataOutputStream stream = new DataOutputStream(o);
+		try {
+			stream.writeShort(this.name);
+			stream.writeShort(this.type);
+			stream.writeShort(this.clase);
+			stream.writeInt(this.ttl);
+			stream.writeShort(this.rdlength);
+			stream.write(this.address.getAddress());
+			
+			return o.toByteArray();
+		}
+		catch(IOException e) {
+			System.out.println("Error creando stream");
+			return null;
+		}
+		
+		
+	}
+
+
 	
 	
 }
--- src/servidorDNS/Servidor.java
+++ src/servidorDNS/Servidor.java
@@ -1,17 +1,42 @@
+/*
+4. MESSAGES
+
+4.1. Format
+
+All communications inside of the domain protocol are carried in a single
+format called a message.  The top level format of message is divided
+into 5 sections (some of which are empty in certain cases) shown below:
+
+    +---------------------+
+    |        Header       |
+    +---------------------+
+    |       Question      | the question for the name server
+    +---------------------+
+    |        Answer       | RRs answering the question
+    +---------------------+
+    |      Authority      | RRs pointing toward an authority
+    +---------------------+
+    |      Additional     | RRs holding additional information
+    +---------------------+
+ */
+
+
 package servidorDNS;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.util.*;
 
 public class Servidor {
 	public static final int PUERTO = 53;
-	String master;
+	private String direccionMaster;
+	private HashMap<String, ArrayList<Respuesta> > cache;
 	private int tam;
 	
 	public Servidor(String master, int tam) {
-		this.master = master;
+		this.direccionMaster = master;
 		this.tam = tam;
 	}
 	
@@ -28,9 +53,13 @@ public class Servidor {
 				System.out.println(solicitud.getAddress().getHostAddress() + ": " + solicitud.getPort());
 				System.out.println("tam : " + solicitud.getLength());
 				
+				
+				//falta crear respuestas
 			}
 		} catch(IOException e) {
 			
 		}
+		
+		socket.close();
 	}
 }
